2d1bc4e6192b7a746bcbc0f3af9198a0294e2605
The-F00L <anonemusdragon@gmail.com>
2020-03-04 23:52:48 +0100

    50%

--- EloadasProject/Eloadas.cs
+++ EloadasProject/Eloadas.cs
@@ -1,8 +1,82 @@
 ﻿using System;
 
-namespace EloadasProject
-{
-    public class Class1
+
+    public class Eloadas
     {
+        private bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama,int helyekSzama)
+        {
+            try
+            {
+                foglalasok =  new bool[sorokSzama, helyekSzama];
+            }
+            catch (ArgumentException e)
+            {
+
+            throw e;
+            }
+        }
+
+        public bool Lefoglal() {
+
+            for (int i = 0; i < foglalasok.Length; i++)
+            {
+                for (int j = 0; j < foglalasok.Length; j++)
+                {
+                    if (foglalasok[i,j]==false)
+                    {
+                       return foglalasok[i, j] = true;
+                    }
+                    
+                }
+            }
+            return false;
+            
+        }
+
+        private int szabadHelyek()
+        {
+            int db = 0;
+            for (int i = 0; i < foglalasok.Length; i++)
+            {
+                for (int j = 0; j < foglalasok.Length; j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        db++;
+                    }
+                }
+            }
+            return db;
+        }
+
+        public int SzabadHelyek { get => szabadHelyek();}
+
+
+        public bool Teli() {
+           
+            for (int i = 0; i < foglalasok.Length; i++)
+            {
+                for (int j = 0; j < foglalasok.Length; j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        return false;   
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        public bool Foglalt(int sorSzam, int helySzam) {
+            if (foglalasok[sorSzam-1,helySzam-1]==false)
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
-}
+
---
+++ EloadasProject/Tests/eloadasTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+
+class eladasCtorTest
+{
+    [TestCase]
+    public void ctorTest() {
+        Eloadas e = new Eloadas(5,5);
+
+    }
+
+    [TestCase]
+    public void Fogtest() {
+        Eloadas e = new Eloadas(5, 5);
+        Assert.AreEqual(true, e.Lefoglal());
+    }
+
+    [TestCase]
+    public void SzabadHelyTest()
+    {
+        Eloadas e = new Eloadas(5, 5);
+        Assert.AreEqual(25,e.SzabadHelyek);
+    }
+
+    [TestCase]
+    public void teli()
+    {
+        Eloadas e = new Eloadas(5, 5);
+        Assert.AreEqual(false,e.Teli());
+    }
+
+    [TestCase]
+    public void Foglalttest()
+    {
+        Eloadas e = new Eloadas(5, 5);
+        Assert.AreEqual(false, e.Foglalt(2,2));
+
+    }
+
+    [TestCase]
+    public void test()
+    {
+        Eloadas e = new Eloadas(5, 5);
+        Assert.AreEqual(true, e.Lefoglal());
+        Assert.AreEqual(25, e.SzabadHelyek);
+        Assert.AreEqual(false, e.Teli());
+        Assert.AreEqual(true, e.Foglalt(2,2));
+       
+
+    }
+
+}
